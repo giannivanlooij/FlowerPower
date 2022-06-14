4fc2ec93a2270b5e2eeef331739db5531749b319
giannivanlooij <gvanlooij@roc-dev.com>
2022-06-14 11:25:36 +0200

    removed the login. caused a mayor problem

--- FlowerPower/routes/web.php
+++ FlowerPower/routes/web.php
@@ -20,9 +20,6 @@ Route::get('/', function () {
 Route::get('/producten', function () {
     return view('components/products/products');
 });
-Route::get('/login',  array('as' => 'login', function() {
-    return view('components/login');
-}));
 
 Route::get('/home', function () {
     return view('components/home');
