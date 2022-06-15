456151488a39e4d32897961c9e1f1d180d56b189
giannivanlooij <gvanlooij@roc-dev.com>
2022-06-15 13:43:54 +0200

    bad commit

--- FlowerPower/resources/views/components/dashboard.blade.php
+++ FlowerPower/resources/views/components/dashboard.blade.php
@@ -1,3 +1,43 @@
 <div>
-    <!-- Well begun is half done. - Aristotle -->
+    <div>
+        <div class="card-columns">
+            <div class="card">
+                <div class="card-body">
+                    <!-- { { A u th : :u se r() -> na m e }}  -->
+                    <h5 class="card-title">Welkom</h5>
+                    <p class="card-text"></p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title"></h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title"></h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title"></h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title"></h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title"></h5>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
